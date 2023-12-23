e37db5e4b2172133ad5f1bf04edf992c27137c54
Jim hill <jim.hill.r@gmail.com>
2023-12-22 17:47:21 -0800

    stuff

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import { Typography} from "@mui/material"
+
+export default function IndexPage() {
+  return (
+    <>
+      <Typography component="h1" variant="h1" align="right" noWrap >
+        Passion Fruit
+      </Typography>
+    </>
+  )
+}
